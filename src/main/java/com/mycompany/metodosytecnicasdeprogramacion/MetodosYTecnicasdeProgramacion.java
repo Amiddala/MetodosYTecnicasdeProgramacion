a59c78d5d6ac80e612558d90d5339dae190acfc9
Candy <168691418+Amiddala@users.noreply.github.com>
2024-09-04 20:35:08 -0400

    efe nomas

--- src/main/java/com/mycompany/metodosytecnicasdeprogramacion/MetodosYTecnicasdeProgramacion.java
+++ src/main/java/com/mycompany/metodosytecnicasdeprogramacion/MetodosYTecnicasdeProgramacion.java
@@ -11,6 +11,6 @@ package com.mycompany.metodosytecnicasdeprogramacion;
 public class MetodosYTecnicasdeProgramacion {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Hello efe nomas World!");
     }
 }
